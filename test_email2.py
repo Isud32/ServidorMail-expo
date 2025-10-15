141b3ebf75834dce852d4680e4641fd3bfbf1f27
Isud <tobias_corrales@hotmail.com>
2025-10-15 12:24:23 -0300

    added new email tec2@tec2.com

--- test_email2.py
+++ test_email2.py
@@ -3,7 +3,7 @@ import requests
 url = "http://192.168.1.32/"
 data = {
     "from": "benjatecnologia@local",
-    "to": "display@server.local",
+    "to": "tec2@tec2.com",
     "subject": "soad",
     "body": "Liar, liar. Banana, banana, banana, terracotta. Banana terracotta, terracotta pie. Banana, banana, banana, terracotta. Banana terracotta, terracotta pie. Is there a perfect way of holding you, baby? (Liar) Vicinity of obscenity in your eyes. Terracotta, terracotta, terracotta pie. Is there a perfect way of holding you, baby? (Liar) Vicinity of obscenity in your eyes. Terracotta pie, hey. Terracotta pie, hey. Terracotta pie, hey. Terracotta pie. Banana, banana, banana, banana, terracotta. Banana terracotta, terracotta pie. Banana, banana, banana, banana, terracotta. Banana terracotta, terracotta pie."
 }
